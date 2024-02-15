784e0ef5abbea359d47f624ecaecf297f5781748
letItCurl <rolandlopez.developer@gmail.com>
2024-02-15 11:36:46 +0700

    1000-rows-optimization-on-index-page.

--- app/controllers/assemblies_controller.rb
+++ app/controllers/assemblies_controller.rb
@@ -60,7 +60,7 @@ class AssembliesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_assembly
-      @assembly = Assembly.find(params[:id])
+      @assembly = Assembly.includes(:parts).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
