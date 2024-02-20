c9e3a0352e4111aaa3e429befaf7174ba3007020
letItCurl <rolandlopez.developer@gmail.com>
2024-02-20 12:48:11 +0700

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+1000.times do |x|
+  assembly = Assembly.create(title: "Asm #{x}", serial: "abc-#{x}-cdf")
+  assembly.parts.create([{title: "PART A #{x}", serial: "xzf-#{x}-idp"}, {title: "PART B #{x}", serial: "xzf-#{x}-idp"}, {title: "PART B #{x}", serial: "xzf-#{x}-idp"}])
+end
